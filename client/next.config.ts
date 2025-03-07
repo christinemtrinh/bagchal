7c32f02f3cb108c9079ec73b45f4387d164f7ece
Chrissy <christinee.trinhh@gmail.com>
2025-03-07 16:37:21 -0600

    3: clean up unused code

--- client/next.config.ts
+++ client/next.config.ts
@@ -11,11 +11,6 @@ const nextConfig = {
 module.exports = {
   async rewrites() {
     return [
-/*       {
-        source: '/api/test', // Explicitly match /api/test
-        destination: 'http://localhost:8080/api/test', // Forward to this URL
-        basePath: false,
-      }, */
       {
         source: '/api/:path*', // Match all other paths starting with /api
         destination: 'http://localhost:8080/api/:path*',
