198031c41ccb95607ef00c5ae1dee4366a53bc30
Chrissy <christinee.trinhh@gmail.com>
2025-03-26 21:59:36 -0500

    Whoops I added an empty file

---
+++ Server/api.js
@@ -0,0 +1,14 @@
+// Determine where a tiger may move
+// Input: Array of integers representing the game board, the index represents the location
+//     0=unoccupied, 1=goat, 2=tiger
+// Output: Dictionary of where each tiger may move
+//     Example: For two tigers on the spots 1 and 5...
+//     { 1: [2, 3, 4]
+//       5: [4, 9]
+//     }
+function getTigerLegalMoves(board) {}             
+
+// Determine if a goat has been captured, using the previous state
+// Input: Array of integers representing the game board, the index is the location
+// Output: Array of integers representing the game board, with the goat removed, if applicable
+function isAnyGoatCaptured(board) {}
