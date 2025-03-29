3ed9b0fcde9145f3ff8c2268ac0afb0b61614c63
Andrew Trinh <andrewwtrinhh@gmail.com>
2025-03-29 02:30:50 -0500

    Added a few more comments

--- Server/api.js
+++ Server/api.js
@@ -8,7 +8,38 @@
 //     }
 function getTigerLegalMoves(board) {}             
 
+//Using output given by getTigerLegalMoves, Restrict movement to those spots and get user input to move tiger
+//Input: Tiger to move, Spot to move
+//Output: Final location, initial location
+function moveTiger(board) {}
+
 // Determine if a goat has been captured, using the previous state
 // Input: Array of integers representing the game board, the index is the location
 // Output: Array of integers representing the game board, with the goat removed, if applicable
 function isAnyGoatCaptured(board) {}
+
+// Determine if there is not 15 goats that have been placed yet, place if not yet at 15
+// Input: Location user pick to place goat
+// Output: Place Goat
+function placeGoat(board) {}
+
+// Determine where a goat may move
+// Input: Array of integers representing the game board, the index represents the location
+//     0=unoccupied, 1=goat, 2=tiger
+// Output: Dictionary of where each goat may move
+function getGoatLegalMoves(board) {}             
+
+//Using output given by getGoatLegalMoves, Restrict movement to those spots and get user input to move tiger
+//Input: Tiger to move, Spot to move
+//Output: Final location, initial location
+function moveGoat(board) {}
+
+// Checks to see if tiger is cornered (No legal spots to move)
+// Input: Tiger to check
+// Output: True/False
+function isTigerCornered(board){}
+
+// Checks to see remaining goats = 0
+// Input: Num of goats
+// Output: True/False
+function isTigerWin(board){}
